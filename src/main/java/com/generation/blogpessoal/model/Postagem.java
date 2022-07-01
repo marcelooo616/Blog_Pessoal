efd01fd42ecd0d52f0ca8065551f37ed0aad14df
marcelooo616 <marcelooo2014@hotmail.com>
2022-07-01 11:28:49 -0300

    Modificando arquivo postagem add classes tema

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,13 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //@Entity transforma o objeto de postagem em uma tabela no banco de dados
 @Entity
@@ -21,55 +22,74 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Table(name = "tb_postagem")
 
 public class Postagem {
-	//@Id define a chave primaria (PRIMARY KEY)
+	// @Id define a chave primaria (PRIMARY KEY)
 	@Id
-	
-	//@GeneratedValue fara o auto_increment no id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
-	private long id;   
-	
-	//@NotNull indica o titulo como campo abrigatorio
+
+	// @GeneratedValue fara o auto_increment no id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	// @NotNull indica o titulo como campo abrigatorio
 	@NotNull
-	
-	//@Size indica a quantitade de caractere do titulo minimo de 5 e no maximo 100
-	@Size(min = 5 , max = 100)
-	private String titulo;  
-	
-	
+
+	// @Size indica a quantitade de caractere do titulo minimo de 5 e no maximo 100
+	@Size(min = 5, max = 100)
+	private String titulo;
+
 	@NotNull
 	@Size(min = 10, max = 500)
-	private String texto; 
-	
+	private String texto;
+
 	// pega automaticamente hora e data do seu computador
-	
-	//@Temporal(TemporalType.TIMESTAMP)
+
+	// @Temporal(TemporalType.TIMESTAMP)
 	@UpdateTimestamp
-	private Date date =  new java.sql.Date(System.currentTimeMillis());   	
-	
-	
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	//declarando a relação entre as tabelas, muitas postagem para um tema
+	@ManyToOne
+	//quando chegar na postagem pare de trazer a informação
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
 		this.date = date;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 }
